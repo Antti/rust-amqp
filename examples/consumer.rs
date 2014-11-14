695cc07a841c04849b4f4fb14a413a69f59fdef3
Andrii Dmytrenko <andrey@reevoo.com>
2014-11-14 16:01:41 +0000

    Add example producer & consumer.

---
+++ examples/consumer.rs
@@ -0,0 +1,40 @@
+extern crate amqp;
+
+use amqp::session::Options;
+use amqp::session::Session;
+use amqp::protocol;
+use amqp::table;
+use amqp::basic::Basic;
+use amqp::channel::Channel;
+use std::default::Default;
+
+
+static mut msg_count: uint = 0;
+
+fn consumer_function(channel: &Channel, deliver: protocol::basic::Deliver, headers: protocol::basic::BasicProperties, body: Vec<u8>){
+    channel.basic_ack(deliver.delivery_tag, false);
+    unsafe {
+        msg_count+= 1;
+        std::io::print(format!("{}\r", msg_count).as_slice());
+    }
+}
+
+fn main() {
+    let mut session = Session::new(Options{.. Default::default()}).unwrap();
+    let mut channel = session.open_channel(1).unwrap();
+    println!("Openned channel: {}", channel.id);
+
+    let queue_name = "test_queue";
+    //ticket: u16, queue: &str, passive: bool, durable: bool, exclusive: bool, auto_delete: bool, nowait: bool, arguments: Table
+    let queue_declare = channel.queue_declare(queue_name, false, true, false, false, false, table::new());
+    println!("Queue declare: {}", queue_declare);
+
+    //queue: &str, consumer_tag: &str, no_local: bool, no_ack: bool, exclusive: bool, nowait: bool, arguments: Table
+    println!("Declaring consumer...");
+    let consumer_name = channel.basic_consume(consumer_function, queue_name, "", false, false, false, false, table::new());
+    println!("Starting consumer {}", consumer_name);
+    channel.start_consuming();
+
+    channel.close(200, "Bye");
+    session.close(200, "Good Bye".to_string());
+}
---
+++ examples/producer.rs
@@ -0,0 +1,29 @@
+extern crate amqp;
+
+use amqp::session::Options;
+use amqp::session::Session;
+use amqp::protocol;
+use amqp::table;
+use amqp::basic::Basic;
+use amqp::channel::Channel;
+use std::default::Default;
+
+
+fn main() {
+    let mut session = Session::new(Options{.. Default::default()}).unwrap();
+    let mut channel = session.open_channel(1).unwrap();
+    println!("Openned channel: {}", channel.id);
+
+    let queue_name = "test_queue";
+    //ticket: u16, queue: &str, passive: bool, durable: bool, exclusive: bool, auto_delete: bool, nowait: bool, arguments: Table
+    let queue_declare = channel.queue_declare(queue_name, false, true, false, false, false, table::new());
+    println!("Queue declare: {}", queue_declare);
+
+    loop {
+        channel.basic_publish("", queue_name, true, false,
+            protocol::basic::BasicProperties{ content_type: Some("text".to_string()), ..Default::default()},
+            (b"Hello from rust!").to_vec());
+    }
+    channel.close(200, "Bye");
+    session.close(200, "Good Bye".to_string());
+}

4ce2f71f707f49fa19e3332f24f485981169e0bd
Andrii Dmytrenko <refresh.xss@gmail.com>
2015-05-18 15:09:39 +0100

    Implement back message acking/nacking/rejecting in basic.

--- src/basic.rs
+++ src/basic.rs
@@ -4,11 +4,20 @@ use table::Table;
 use framing::{ContentHeaderFrame, FrameType, Frame};
 use protocol::{MethodFrame, basic, Method};
 use protocol::basic::{BasicProperties, GetOk, Consume, ConsumeOk, Deliver, Publish, Ack, Nack, Reject};
+use std::sync::mpsc::{sync_channel, Receiver, SyncSender};
+
+enum AckAction {
+    Ack(u64),
+    Nack(u64, bool),
+    Reject(u64, bool)
+}
 
 pub struct GetIterator <'a> {
     queue: &'a str,
     no_ack: bool,
-    pub channel: &'a mut Channel
+    channel: &'a mut Channel,
+    ack_receiver: Receiver<AckAction>,
+    ack_sender: SyncSender<AckAction>
 }
 
 pub trait Basic <'a> {
@@ -25,16 +34,18 @@ pub trait Basic <'a> {
 }
 
 // #[derive(Debug)]
-pub struct GetResult{
+pub struct GetResult {
     pub reply: GetOk,
     pub headers: BasicProperties,
-    pub body: Vec<u8>
+    pub body: Vec<u8>,
+    ack_sender: SyncSender<AckAction>
 }
 
 impl <'a> Iterator for GetIterator<'a > {
     type Item = GetResult;
 
     fn next(&mut self) -> Option<Self::Item> {
+        self.ack_message();
         let get = &basic::Get{ ticket: 0, queue: self.queue.to_string(), no_ack: self.no_ack };
         let method_frame = self.channel.raw_rpc(get);
         match method_frame.method_name() {
@@ -43,11 +54,43 @@ impl <'a> Iterator for GetIterator<'a > {
                 let headers = self.channel.read_headers().ok().unwrap();
                 let body = self.channel.read_body(headers.body_size).ok().unwrap();
                 let properties = BasicProperties::decode(headers).ok().unwrap();
-                Some(GetResult {headers: properties, reply: reply, body: body})
+                Some(GetResult {headers: properties, reply: reply, body: body, ack_sender: self.ack_sender.clone()})
             }
             "basic.get-empty" => None,
             method => panic!(format!("Not expected method: {}", method))
         }
+    }
+}
+
+/// Will acknowledge
+
+impl <'a> Drop for GetIterator <'a> {
+    fn drop(&mut self) {
+        self.ack_message();
+    }
+}
+
+impl <'a> GetIterator <'a> {
+    fn ack_message(&mut self) {
+        self.ack_receiver.try_recv().map(|ack_action|
+            match ack_action {
+                AckAction::Ack(delivery_tag) => self.channel.basic_ack(delivery_tag, false),
+                AckAction::Nack(delivery_tag, requeue) => self.channel.basic_nack(delivery_tag, false, requeue),
+                AckAction::Reject(delivery_tag, requeue) => self.channel.basic_reject(delivery_tag, requeue)
+            }
+        ).unwrap_or(());
+    }
+}
+
+impl GetResult {
+    pub fn ack(&self) {
+        self.ack_sender.try_send(AckAction::Ack(self.reply.delivery_tag)).unwrap_or(());
+    }
+    pub fn nack(&self, requeue: bool) {
+        self.ack_sender.try_send(AckAction::Nack(self.reply.delivery_tag, requeue)).unwrap_or(());
+    }
+    pub fn reject(&self, requeue: bool) {
+        self.ack_sender.try_send(AckAction::Reject(self.reply.delivery_tag, requeue)).unwrap_or(());
     }
 }
 
@@ -68,12 +111,13 @@ impl <'a> Basic<'a> for Channel {
     ///     println!("Headers: {:?}", get_result.headers);
     ///     println!("Reply: {:?}", get_result.reply);
     ///     println!("Body: {:?}", String::from_utf8_lossy(&get_result.body));
-    ///     //get_result.channel.basic_ack(get_result.reply.delivery_tag, false);
+    ///     get_result.ack();
     /// }
     /// ```
     ///
     fn basic_get(&'a mut self, queue: &'a str, no_ack: bool) -> GetIterator<'a> {
-        GetIterator { channel: self, queue: queue, no_ack: no_ack }
+        let (tx, rx) = sync_channel::<AckAction>(1);
+        GetIterator { channel: self, queue: queue, no_ack: no_ack, ack_receiver: rx, ack_sender: tx }
     }
 
     fn basic_consume(&mut self, callback: ConsumerCallback,

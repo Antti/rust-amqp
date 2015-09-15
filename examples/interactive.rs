5b35fd09d28479394f8f3a154af4d7823a519a3f
Luca Bruno <lucab@debian.org>
2015-09-15 23:13:03 +0200

    examples: add a simple interactive example
    Adding a really basic interactive example, which get some data from
    stdio, put it into a queue and try to read it back again.
    Also the AMQP endpoint can be specified by the user.
    
    This comes useful to quickly test new features and try the library
    against remote endpoints.
    
    Signed-off-by: Luca Bruno <lucab@debian.org>

---
+++ examples/interactive.rs
@@ -0,0 +1,42 @@
+extern crate amqp;
+extern crate env_logger;
+
+use amqp::session::Session;
+use amqp::protocol;
+use amqp::table;
+use amqp::basic::Basic;
+use std::default::Default;
+use std::io;
+
+fn main() {
+    env_logger::init().unwrap();
+    let mut amqp_url = String::new();
+    println!("Please input the AMQP endpoint to use: ");
+    io::stdin().read_line(&mut amqp_url).ok().expect("Unable to read URL!");
+    let mut session = match Session::open_url(&amqp_url[..]) {
+        Ok(session) => session,
+        Err(error) => panic!("Can't create session: {:?}", error)
+    };
+    let mut channel = session.open_channel(1).ok().expect("Can't open channel!");
+
+    let queue_name = "test_queue";
+    channel.queue_declare(queue_name, false, true, false, false, false, table::new()).ok().expect("Unable to declare queue!");
+
+    println!("Type some text here below, it will be sent into the '{}' queue. Hit Enter when done.", queue_name);
+    let mut input_data = String::new();
+    io::stdin().read_line(&mut input_data).ok().expect("Unable to read input data!");
+
+    println!("Sending data...\n");
+    channel.basic_publish("", queue_name, true, false,
+        protocol::basic::BasicProperties{ content_type: Some("text".to_string()), ..Default::default()},
+        input_data.trim_right().to_string().into_bytes());
+
+     for get_result in channel.basic_get(queue_name, false) {
+        println!("Received: {:?}", String::from_utf8_lossy(&get_result.body));
+        get_result.ack();
+    }
+
+    println!("Queue is now empty, quitting...");
+    channel.close(200, "Bye".to_string());
+    session.close(200, "Good Bye".to_string());
+}

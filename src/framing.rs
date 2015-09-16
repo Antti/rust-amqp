296c8435ba6a1f4a6822cc2e208ad50f57c44f18
Andrii Dmytrenko <refresh.xss@gmail.com>
2015-09-16 11:02:05 +0100

    Comments about slices casts.

--- src/framing.rs
+++ src/framing.rs
@@ -25,21 +25,25 @@ pub struct Frame {
 
 impl Frame {
     pub fn decode<T: Read>(reader: &mut T) -> AMQPResult<Frame> {
-        let mut header : &mut [u8] = &mut [0u8; 7];
+        // cast sized [u8] to unsized
+        let mut header = &mut [0u8; 7] as &mut [u8];
         try!(reader.read(&mut header));
-        let mut header : &[u8] = header;
-        let header : &mut &[u8] = &mut header;
+        // Make a &mut to &[u8]. &mut &[u8] implements `Read` trait.
+        // `Read` works by changing a mutable reference to immutable slice,
+        // as in with a basic pointer manipulation.
+        let header = &mut (header as &[u8]) as &mut &[u8];
         let frame_type_id = try!(header.read_u8());
         let channel = try!(header.read_u16::<BigEndian>());
         let size = try!(header.read_u32::<BigEndian>()) as usize;
+        // We need to use Vec because the size is not know in compile time.
         let mut payload: Vec<u8> = iter::repeat(0u8).take(size).collect();
         try!(reader.read(&mut payload));
         let frame_end = try!(reader.read_u8());
         if payload.len() != size {
-            return Err(AMQPError::DecodeError("Payload didn't read the full size"));
+            return Err(AMQPError::DecodeError("Cannot read a full frame payload"));
         }
         if frame_end != 0xCE {
-            return Err(AMQPError::DecodeError("Frame end wasn't right"));
+            return Err(AMQPError::DecodeError("Frame didn't end with 0xCE"));
         }
         let frame_type = match FrameType::from_u8(frame_type_id){
             Some(ft) => ft,

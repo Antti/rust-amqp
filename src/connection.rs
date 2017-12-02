86b0b7f7b4f9b364e53d156941ed358473a70a0f
Graham Christensen <graham@grahamc.com>
2017-12-02 08:28:00 -0500

    Update connection.rs

--- src/connection.rs
+++ src/connection.rs
@@ -24,7 +24,7 @@ impl Connection {
     #[cfg(feature = "tls")]
     pub fn open_tls(host: &str, port: u16) -> AMQPResult<Connection> {
         let socket = try!(TcpStream::connect((host, port)));
-        socket.set_read_timeout(Some(time::Duration::from_secs(1)));
+        socket.set_read_timeout(Some(time::Duration::from_millis(250)));
         let connector = SslConnectorBuilder::new(SslMethod::tls()).unwrap().build();
 
         let mut tls_socket = try!(connector.connect(host, socket));
